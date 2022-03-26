ccec5f739eb9e3e68254e99ff1e641675d70e87d
sungju-kim <wnwn0719@gmail.com>
2022-03-27 00:34:33 +0900

    solve p2448()

---
+++ Algorithm_study/Algorithm_study/재귀/2448_별찍기11.swift
@@ -0,0 +1,30 @@
+//
+//  2448_별찍기11.swift
+//  Algorithm_study
+//
+//  Created by dale on 2022/03/26.
+//
+
+import Foundation
+func p2448() {
+    let n = Int(readLine()!)!
+    let triangle = reculsive(3, ["*","* *","*****"])
+    
+    for i in 0..<triangle.count {
+        let front = String(repeating: " ", count: n-1-i)
+        print("\(front)\(triangle[i])\(front)")
+    }
+    func reculsive(_ m: Int, _ triangle: [String]) -> [String]{
+        if m == n {
+            return triangle
+        }
+        var temp : [String] = triangle
+        var count = m*2-1
+        for i in 0..<triangle.count {
+            let blank : String = String(repeating: " ", count: count)
+            count -= 2
+            temp.append("\(triangle[i])\(blank)\(triangle[i])")
+        }
+        return reculsive(2 * m, temp)
+    }
+}
