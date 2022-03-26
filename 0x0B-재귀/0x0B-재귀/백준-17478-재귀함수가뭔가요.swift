e53ffb96165aa272bd6e9af9534d07fe53d5a881
sanghyeok-kim <slsnsi1212@gmail.com>
2022-03-26 20:26:09 +0900

    [Solve] 백준-17478-재귀함수가뭔가요

--- 0x0B-재귀/0x0B-재귀/백준-17478-재귀함수가뭔가요.swift
+++ 0x0B-재귀/0x0B-재귀/백준-17478-재귀함수가뭔가요.swift
@@ -7,60 +7,42 @@
 
 import Foundation
 
-let count = Int(readLine()!)!
-let inputCount = count
+let indentCount = Int(readLine()!)!
+let inputCount = indentCount
 
-func indent(count: Int, str: String) -> String {
-    let indent = String(repeating: "_", count: count * 4)
-    let content = "\(indent)\(str)"
+func makeString(indentCount: Int, string: String) -> String {
+    let indent = String(repeating: "_", count: indentCount * 4)
+    let content = "\(indent)\(string)"
     
     return content
 }
 
-let 재귀무엇 = """
-"재귀함수가 뭔가요?"
-"""
+let 재귀무엇 = "\"재귀함수가 뭔가요?\""
+let 잘듣 = "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어."
+let 마을 = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지."
+let 답옳 = "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\""
+let 재귀자신 = "\"재귀함수는 자기 자신을 호출하는 함수라네\""
+let 라고할뻔 = "라고 답변하였지."
 
-let 잘듣 = """
-"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.
-"""
-
-let 마을 = """
-마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.
-"""
-
-let 답옳 = """
-그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."
-"""
-
-let 재귀자신 = """
-"재귀함수는 자기 자신을 호출하는 함수라네"
-"""
-
-let 라고할뻔 = """
-라고 답변하였지.
-"""
-
-func recursive(count: Int) {
-    let k = inputCount - count
+func recursive(recursionCount: Int) {
+    let indentCount = inputCount - recursionCount
     
-    print(indent(count: k, str: 재귀무엇))
+    print(makeString(indentCount: indentCount, string: 재귀무엇))
     
-    if count == 0 {
-        print(indent(count: k, str: 재귀자신))
-        print(indent(count: k, str: 라고할뻔))
+    if recursionCount == 0 {
+        print(makeString(indentCount: indentCount, string: 재귀자신))
+        print(makeString(indentCount: indentCount, string: 라고할뻔))
         return
     }
     
+    print(makeString(indentCount: indentCount, string: 잘듣))
+    print(makeString(indentCount: indentCount, string: 마을))
+    print(makeString(indentCount: indentCount, string: 답옳))
     
-    print(indent(count: k, str: 잘듣))
-    print(indent(count: k, str: 마을))
-    print(indent(count: k, str: 답옳))
+    recursive(recursionCount: recursionCount - 1)
     
-    recursive(count: count - 1)
-    
-    print(indent(count: k, str: 라고할뻔))
+    print(makeString(indentCount: indentCount, string: 라고할뻔))
 }
 
 print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
-recursive(count: count)
+recursive(recursionCount: indentCount)
