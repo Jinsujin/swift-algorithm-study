9f838048b4a42d066b22ed8778152f1f133d1a76
sungju-kim <wnwn0719@gmail.com>
2022-04-03 18:00:46 +0900

    try - 1753_최단경로

--- Algorithm_study/Algorithm_study/main.swift
+++ Algorithm_study/Algorithm_study/main.swift
@@ -27,6 +27,5 @@ import Foundation
 //p2447()
 //print(nExpression(2, 1))
 //print(targetNumber([4,1,2,1], 4))
-print(convertWord("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
-print(convertWord("hit", "cog",["cog", "log", "lot", "dog", "dot", "hot"]))
-
+//print(convertWord("hit", "cog",["cog", "log", "lot", "dog", "dot", "hot"]))
+p1753()
