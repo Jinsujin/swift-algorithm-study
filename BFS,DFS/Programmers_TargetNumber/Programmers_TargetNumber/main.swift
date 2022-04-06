36db4b377357395070946891a831cc57e69041aa
P_SeoB <akdlsjtm21@naver.com>
2022-04-06 18:28:19 +0900

    sovle Target Number

--- BFS,DFS/Programmers_TargetNumber/Programmers_TargetNumber/main.swift
+++ BFS,DFS/Programmers_TargetNumber/Programmers_TargetNumber/main.swift
@@ -13,22 +13,25 @@ import Foundation
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     var result:Int = 0
-    var sum:Int = 0
     
-    func dfs(index: Int, sum:Int) {
-        if index == numbers.count {
+    func dfs(depth: Int, sum:Int) {
+        if depth == numbers.count {
             if sum == target {
                 result += 1
             }
+            return
         }
+        
+        dfs(depth: depth + 1, sum: sum + numbers[depth])
+        dfs(depth: depth + 1, sum: sum - numbers[depth])
     }
-
+    
+    dfs(depth: 0, sum: 0)
     
     
     
     return result
 }
-
 
 
 
